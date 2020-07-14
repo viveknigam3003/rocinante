c4405e4aba8c77c535300e80e8a9b5f502abef66
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-14 19:54:24 +0530

    Added Electron Window

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+let window = undefined;
+
+app.on("ready", () => {
+  createWindow();
+});
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    fullscreenable: true,
+    resizable: true,
+    transparent: false,
+    skipTaskbar: true,
+    scrollable: false,
+    backgroundColor: "#fffafa",
+    useContentSize: true,
+    webPreferences: {
+      backgroundThrottling: false,
+    },
+  });
+
+  window.loadURL("http://localhost:3001");
+};
+
+ipcMain.on("show-window", () => {
+  window.show();
+});

cf8cc876929de133f16282001fe75a2bd44cd7a8
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-15 18:16:29 +0530

    Added Sidebar and Content Layout

---
+++ src/components/Common/Sidebar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles, Toolbar, List, ListItem } from "@material-ui/core";
+import Logo from "../../layout/Logo";
+
+const useStyles = makeStyles({
+  drawer: {
+    width: (props) => props.drawerWidth,
+    flexShrink: 0,
+    backgroudColor: "#3e55ab",
+  },
+  drawerPaper: {
+    width: (props) => props.drawerWidth,
+    backgroundColor: "#3e55ab",
+  },
+  drawerContainer: {
+    overflow: "auto",
+  },
+  logo: {
+    height: 36,
+    padding: 12,
+  },
+  listItem: {
+    padding: "12px 36px",
+    lineHeight: 1.5,
+    letterSpacing: "0.001em",
+    fontFamily: "Cern",
+    fontWeight: 500,
+    color: "#fffafa",
+    fontSize: 16,
+  },
+});
+
+/**
+ * Renders a Sidebar in the app.
+ * @param {any} props Takes in Props
+ * @prop {number} width Defines the sidebar width.
+ */
+function Sidebar(props) {
+  const classes = useStyles({ drawerWidth: props.width });
+  const values = ["Explore", "Storage", "Rules", "Monitoring"];
+
+  return (
+    <React.Fragment>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <Toolbar>
+          <Logo className={classes.logo} />
+        </Toolbar>
+        <List>
+          {values.map((text) => (
+            <ListItem className={classes.listItem} button key={text}>
+              {text}
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </React.Fragment>
+  );
+}
+
+export default Sidebar;

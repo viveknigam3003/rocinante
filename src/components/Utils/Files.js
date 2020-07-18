51233ae088c40a5ba82e7e7a8b95e7a6ad26d280
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-18 13:30:58 +0530

    Returning Response from Request

--- src/components/Utils/Files.js
+++ src/components/Utils/Files.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 export function lsFolder(folder) {
-  axios
-    .post("http://localhost:3004/getfiles", {
+  try {
+    const response = axios.post("http://localhost:3004/getfiles", {
       folder,
       headers: {
         "Content-Type": "application/json",
       },
-    })
-    .then((res) => console.log(res.data.ls))
-    .catch((err) => console.log(err));
+    });
+
+    return response;
+  } catch (err) {
+    return console.log(err);
+  }
 }

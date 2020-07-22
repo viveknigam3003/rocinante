52ffe6d4570fd113971ef9730dbe4bc3867db155
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-23 00:19:59 +0530

    Added Config Utils

---
+++ src/components/Utils/Config.js
@@ -0,0 +1,37 @@
+import { getCurrentAccountConfig } from "./User";
+import { Cookies } from "react-cookie";
+import axios from "axios";
+const cookies = new Cookies();
+
+/**
+ * GETs all the Config sections from the `server`. Works only for ADMIN accounts
+ * @param {String} server Rucio Host  
+ */
+export function getConfig(server) {
+  const currentAccountConfig = getCurrentAccountConfig(server);
+
+  const payload = {
+    certlocation: currentAccountConfig.certlocation,
+    serverHost: currentAccountConfig.server,
+    token: "",
+  };
+
+  try {
+    payload.token = cookies.get(currentAccountConfig.server.name);
+  } catch {
+    return 401;
+  }
+
+  try {
+    const response = axios.post("/config", {
+      payload,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response;
+  } catch {
+    return 500;
+  }
+}

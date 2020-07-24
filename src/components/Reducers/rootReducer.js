44de610864d2ca4b39009e60c23b2b14d6f50c3a
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-24 13:20:23 +0530

    Adding Login and Status Reducers

--- src/components/Reducers/rootReducer.js
+++ src/components/Reducers/rootReducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import directoryReducer from "./directoryReducer";
 import metadataReducer from "./metadataReducer";
+import loginReducer from "./loginReducer";
+import statusReducer from "./statusReducer";
 
 const rootReducer = combineReducers({
     directory: directoryReducer,
-    metadata: metadataReducer
+    metadata: metadataReducer,
+    login: loginReducer,
+    status: statusReducer
 })
 
 export default rootReducer;

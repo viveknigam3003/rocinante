f346d923dd887dd569d41f6f90cb804e6507c40b
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-29 14:39:42 +0530

    Adding more config routes

--- server/APIs/config.js
+++ server/APIs/config.js
@@ -12,4 +12,37 @@ async function getAllConfig(certlocation, server, token) {
     .then(console.log(`[INFO] Config received for ${server.name}`));
 }
 
-module.exports = getAllConfig;
+async function addConfig(certlocation, server, token, payload) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation), rejectUnauthorized: false });
+  const section = payload.section;
+  const option = payload.option;
+  const value = payload.value;
+
+  return axios
+    .put(`https://${server.host}/config/${section}/${option}/${value}`, {
+      httpsAgent,
+      headers: { "X-Rucio-Auth-Token": token },
+    })
+    .then((res) => {
+      console.log(`[INFO] Added ${section}.${option} on ${server.name}`);
+      console.log(res);
+    });
+}
+
+async function delConfig(certlocation, server, token, payload) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  const section = payload.section;
+  const option = payload.option;
+
+  return axios
+    .delete(`https://${server.host}/config/${section}/${option}`, {
+      headers: { "X-Rucio-Auth-Token": token },
+      httpsAgent,
+    })
+    .then((res) => {
+      console.log(`[INFO] Deleted ${section}.${option} on ${server.name}`);
+      console.log(`[INFO] Response from server: ${res}`);
+    });
+}
+
+module.exports = { getAllConfig, addConfig, delConfig };
--- server/routes/config.js
+++ server/routes/config.js
@@ -1,23 +1,50 @@
 const express = require("express");
-const getAllConfig = require("../APIs/config");
+const config = require("../APIs/config");
 const router = express.Router();
 
 router.post("/config", async (req, res) => {
-    const payload = req.body.payload;
-  
-    await getAllConfig(
-      payload.certlocation,
-      payload.server,
-      payload.token,
-    )
-      .then((config) => {
-        res.send(config.data);
-      })
-      .catch((err) => {
-        console.log(`[ERROR] ` + err);
-        res.sendStatus(401);
-      });
-  });
-  
-  module.exports = router;
-  
+  const payload = req.body.payload;
+
+  await config.getAllConfig(payload.certlocation, payload.server, payload.token)
+    .then((config) => {
+      res.send(config.data);
+    })
+    .catch((err) => {
+      console.log(`[ERROR] ` + err);
+      res.sendStatus(401);
+    });
+});
+
+router.post("/addconfig", async (req, res) => {
+  const payload = req.body.payload;
+
+  await config.addConfig(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.values
+  )
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log(`[ERROR] ${err}`);
+      res.sendStatus(500);
+    });
+});
+
+router.post("/delconfig", async (req, res) => {
+  const payload = req.body.payload;
+
+  await config.delConfig(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.values
+  )
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log(`[ERROR] ${err}`);
+      res.sendStatus(401);
+    });
+});
+
+module.exports = router;

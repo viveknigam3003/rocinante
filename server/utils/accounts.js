6717359374ecf8452ee4f0818cb23a59cf24c6aa
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-31 18:37:11 +0530

    Adding UserCFG routes and API

--- server/utils/accounts.js
+++ server/utils/accounts.js
@@ -28,56 +28,7 @@ function currentUserAuthenticated(currentUser, attemptStatus) {
   return false;
 }
 
-/**
- * Creates a `.cfg` file for JSON data
- * @param {String} filepath 
- * @param {String} block CFG File Block eg: client, database etc
- * @param {Object} params JS Object
- */
-function writeConfigFile(filepath, block, params) {
-  const fs = require("fs");
-
-  fs.writeFile(filepath, `[${block}] \n`, (err) => {
-    if (err) console.log(err);
-  });
-
-  Object.keys(params).forEach((item) => {
-    fs.appendFile(filepath, `${item}: ${params[item]} \n`, (err) => {
-      if (err) console.log(err);
-    });
-  });
-}
-
-/**
- * Takes in `.cfg` file and converts it into `.json`
- * @param {String} filepath 
- */
-function parseConfigFile(filepath) {
-  const fs = require("fs");
-  let lines = [];
-  const obj = {};
-
-  function cleanString(string) {
-    return string.trim().replace("https://", "");
-  }
-
-  fs.readFile(filepath, "utf8", (err, data) => {
-    if (err) console.log(err);
-    lines = data.trim().split(/\r?\n/).splice(1);
-    lines.forEach((line) => {
-      const cleanedLine = cleanString(line);
-      const [key, value] = cleanedLine.split(":");
-      obj[key] = value.trim();
-    });
-    fs.writeFile(filepath + ".json", JSON.stringify(obj), (error) => {
-      if (error) console.log(error)
-    })
-  });
-}
-
 module.exports = {
   validateAccount,
   currentUserAuthenticated,
-  writeConfigFile,
-  parseConfigFile,
 };

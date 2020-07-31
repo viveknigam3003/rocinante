b9905d265087b77728d8eded8311cd1bdf8884f4
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-31 18:37:37 +0530

    Adding usercfg middleware

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const files = require("./routes/files");
 const metadata = require("./routes/metadata");
 const auth = require("./routes/auth");
 const config = require("./routes/config");
+const usercfg = require("./routes/usercfgs");
 
 app.use(cors({ origin: "http://localhost:3005" }));
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(auth);
 app.use(files);
 app.use(metadata);
 app.use(config);
+app.use(usercfg);
 
 app.listen(port, () =>
   console.log(`[INFO] Rucio running at http://localhost:${port}`)

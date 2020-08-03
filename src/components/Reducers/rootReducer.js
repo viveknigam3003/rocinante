0817239c74be7b38bbdbb8421c8bf453f3a281c3
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-04 02:42:33 +0530

    Added Snackbar reducer

--- src/components/Reducers/rootReducer.js
+++ src/components/Reducers/rootReducer.js
@@ -3,12 +3,14 @@ import directoryReducer from "./directoryReducer";
 import metadataReducer from "./metadataReducer";
 import loginReducer from "./loginReducer";
 import statusReducer from "./statusReducer";
+import { snackbarReducer } from "./snackbarReducer";
 
 const rootReducer = combineReducers({
-    directory: directoryReducer,
-    metadata: metadataReducer,
-    login: loginReducer,
-    status: statusReducer
-})
+  directory: directoryReducer,
+  metadata: metadataReducer,
+  login: loginReducer,
+  status: statusReducer,
+  snackbar: snackbarReducer,
+});
 
 export default rootReducer;

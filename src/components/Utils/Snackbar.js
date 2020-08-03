5536b49289e2bcca0fcfa0bb280f456fa4e050b1
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-04 02:53:10 +0530

    Updated snackbar

--- src/components/Utils/Snackbar.js
+++ src/components/Utils/Snackbar.js
@@ -19,9 +19,9 @@ const useStyles = makeStyles((theme) => ({
  * React Component to Display Alert Messages depending on severity.
  * 
  * @param {*} props From Parent Component 
- * @property {boolean} open To handle open and close of Snackbar
  * @property {String} severity can be selected from {"success", "error", "info", "warning"}
  * @property {String} message Message to be displayed
+ * @property {func} onExited Function to perform when the snackbar has exited
  */
 export default function AlertSnackbar(props) {
   const classes = useStyles();
@@ -42,7 +42,6 @@ export default function AlertSnackbar(props) {
 }
 
 AlertSnackbar.propTypes = {
-  open: PropTypes.bool,
   severity: PropTypes.string,
   message: PropTypes.string,
   onExited: PropTypes.func
